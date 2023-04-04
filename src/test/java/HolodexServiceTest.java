09068c891fbc48a1869f2c7eac365d3d95e0d426
Pinapelz <donaldshan1@outlook.com>
2023-04-04 16:17:05 -0700

    Added testing framework setup

---
+++ src/test/java/HolodexServiceTest.java
@@ -0,0 +1,13 @@
+import com.pina.HolodexService;
+import com.pina.LiveStream;
+import org.junit.jupiter.api.Test;
+
+public class HolodexServiceTest {
+
+    @Test
+    public void testGetLiveStreams() throws Exception {
+        //TOOD: Implement testcase using mocking
+    }
+
+
+}
