3057c2d512fb8d6503f199227005c9190ec3164c
Pinapelz <donaldshan1@outlook.com>
2023-04-05 21:16:07 -0700

    Fixed missing return values from certain queries

--- src/main/java/com/pina/datatypes/Channel.java
+++ src/main/java/com/pina/datatypes/Channel.java
@@ -5,10 +5,19 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Channel {
     public String id;
-    public String english_name;
     public String name;
+    public String english_name;
     public String org;
     public String suborg;
     public String photo;
-    public String type;
+    public String banner;
+    public String twitter;
+    public String video_count;
+    public String subscriber_count;
+    public String view_count;
+    public String clip_count;
+    public String lang;
+    public String published_at;
+    public boolean inactive;
+    public String description;
 }
--- src/main/java/com/pina/datatypes/Video.java
+++ src/main/java/com/pina/datatypes/Video.java
@@ -3,21 +3,31 @@ package com.pina.datatypes;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+import java.util.List;
+
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Video {
     public String id;
     public String title;
     public String type;
-    public String topic;
+    public String topic_id;
     public String published_at;
     public String available_at;
     public int duration;
     public String status;
     public String start_scheduled;
     public String start_actual;
-    public String thumbnail;
+    public String end_actual;
     public int live_viewers;
+    public String description;
+    public int songcount;
+    public String channel_id;
+    public List<Video> clips;
+    public List<Video> refers;
+    public List<Channel> mentions;
+    public String thumbnail;
     public Channel channel;
+    public int songs;
 }
 
 
