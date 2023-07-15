916fb7917346643aefe3ad5083a2d920cfd96afc
Pinapelz <donaldshan1@outlook.com>
2023-07-15 00:07:38 -0700

    fix: add missing "oldest" enum to Sort

--- src/main/java/com/pinapelz/vtuber/Sort.java
+++ src/main/java/com/pinapelz/vtuber/Sort.java
@@ -4,7 +4,8 @@ public enum Sort {
     NEWEST("newest"),
     START_SCHEDULED("start_scheduled"),
     START_ACTUAL("start_actual"),
-    END_ACTUAL("end_actual");
+    END_ACTUAL("end_actual"),
+    OLDEST("oldest");
 
     private final String value;
     Sort(String value) {
