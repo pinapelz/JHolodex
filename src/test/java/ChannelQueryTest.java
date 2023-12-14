27de9dd4f19b8e3a1285691b81b600044d3411e8
Pinapelz <yukais@pinapelz.com>
2023-12-13 21:51:08 -0800

    bump junit version. Add initial ChannelQueryTests - Testing commit for gh-actions
    Signed-off-by: Pinapelz <yukais@pinapelz.com>

---
+++ src/test/java/ChannelQueryTest.java
@@ -0,0 +1,47 @@
+import com.pinapelz.datatypes.Channel;
+import org.junit.Test;
+import com.pinapelz.query.ChannelQueryBuilder;
+import com.pinapelz.Holodex;
+import com.pinapelz.HolodexException;
+
+import java.util.List;
+
+import static org.junit.Assert.fail;
+
+public class ChannelQueryTest {
+    private final String apiKey = System.getenv("HOLODEX_API_KEY");
+    @Test
+    public void testChanneQueryIntiailization(){
+        ChannelQueryBuilder channelQueryBuilder = new ChannelQueryBuilder().setLang("en").setOrg("hololive");
+        assert(channelQueryBuilder.getLang().equals("en"));
+        assert(channelQueryBuilder.getOrg().equals("hololive"));
+    }
+
+    @Test
+    public void testChannelQuerySetters(){
+        ChannelQueryBuilder channelQueryBuilder = new ChannelQueryBuilder();
+        channelQueryBuilder.setLang("en");
+        channelQueryBuilder.setOrg("hololive");
+        channelQueryBuilder.setOrder("asc");
+        assert(channelQueryBuilder.getLang().equals("en"));
+        assert(channelQueryBuilder.getOrg().equals("hololive"));
+        assert(channelQueryBuilder.getOrder().equals("asc"));
+    }
+
+    @Test
+    public void getChannelsData(){
+        try {
+            Holodex holodex = new Holodex(apiKey);
+            holodex.getChannels(new ChannelQueryBuilder().setOrg("Hololive"));
+        } catch (HolodexException ex) {
+            fail(ex.getMessage());
+        }
+        assert true;
+    }
+
+
+
+
+
+
+}
